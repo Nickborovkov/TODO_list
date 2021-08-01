2714c54edde32b1102734540d5fb9d55acb08436
Nikita Borovkov <borovckov@yandex.ru>
2021-08-01 18:50:50 +0300

    changed variables declarations

--- src/utils/formHelpers/formControls.jsx
+++ src/utils/formHelpers/formControls.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-let Input = ({input, meta, ...props}) => {
-    let hasError = meta.touched && meta.error
+const Input = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
     return <div>
         <input {...input} {...props}/>
         <div>
--- src/utils/formHelpers/validators.js
+++ src/utils/formHelpers/validators.js
@@ -1,9 +1,9 @@
-export let required = (value) => {
+export const required = (value) => {
     if (!value) return `Goal can't be empty`
     return undefined
 }
 
-export let maxLength = (value) => {
+export const maxLength = (value) => {
     if(value.length > 300) return `Max length is 300 symbols`
     return undefined
 }
