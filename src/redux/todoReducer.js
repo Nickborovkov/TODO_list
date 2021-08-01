098c8bbd0bf0521def619019d9ba33319ea034b7
Nikita Borovkov <borovckov@yandex.ru>
2021-08-01 18:49:46 +0300

    refactored, changed variables declarations, addItems logic moved here from component

--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -12,70 +12,115 @@ const initialState = {
 const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_ITEMS:
-            let storageArray = [...state.items]
+            //Getting items from localStorage
+            const newItemsArray = []
             for (let i = 1; i <= localStorage.length; i ++){
-                let resultString = JSON.parse(localStorage.getItem(String(i)))
-                storageArray.push(
-                    {id: resultString[0], itemText: resultString[1],
-                        isCompleted: resultString[2],  currentDate: resultString[3]}
+                const resultString = JSON.parse(localStorage.getItem(String(i)))
+                newItemsArray.push(
+                    {id: resultString[0],
+                        itemText: resultString[1],
+                        isCompleted: resultString[2],
+                        currentDate: resultString[3]}
                 )
             }
             return {
+                //Setting items to UI
                 ...state,
-                items: storageArray
+                items: newItemsArray
             }
         case ADD_ITEM:
+            const itemsArray = [...state.items]
+            //Getting date when item was added
+            const months = [`Jan`,`Feb`,`Mar`,`Apr`,`May`,`Jun`,`Jul`,`Aug`,`Sep`,`Oct`,`Nov`,`Dec`]
+            const days = [`Mon`,`Tue`,`Wed`,`Thu`,`Fri`,`Sat`,`Sun`]
+            const now = new Date()
+            const newDate = `${months[now.getMonth()]} ${days[now.getDay()]} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+            //Setting new item to localStorage
+            localStorage.setItem(`${itemsArray.length + 1}`,
+                JSON.stringify(
+                    [itemsArray.length + 1,
+                    action.itemText,
+                    false,
+                    newDate])
+                )
             return {
+                //Adding items to UI
                 ...state,
-                items: [...state.items, {id: state.items.length + 1,
+                items: [...state.items, {id: itemsArray.length + 1,
                     itemText: action.itemText,
                     isCompleted :false,
-                    currentDate: action.currentDate
+                    currentDate: newDate
                 }],
             }
         case DELETE_ITEM:
-            let newResultArray = []
+            let itemsDeleteArray = []
+            //Getting items from localStorage
             for (let i = 1; i <= localStorage.length; i++){
-                let resultString = JSON.parse(localStorage.getItem(String(i)))
+                const resultString = JSON.parse(localStorage.getItem(String(i)))
                 if(resultString[0] !== action.itemId){
-                    newResultArray.push({id: newResultArray.length + 1, itemText: resultString[1],
-                        isCompleted: resultString[2],  currentDate: resultString[3]})
-                                    }
+                    itemsDeleteArray.push({id: itemsDeleteArray.length + 1,
+                        itemText: resultString[1],
+                        isCompleted: resultString[2],
+                        currentDate: resultString[3]})
+                }
             }
+            //Clearing localStorage
             localStorage.clear()
-            for (let i = 0; i <= newResultArray.length - 1; i++){
-                localStorage.setItem(`${newResultArray[i].id}`, JSON.stringify([newResultArray[i].id, newResultArray[i].itemText, newResultArray[i].isCompleted, newResultArray[i].currentDate]))
+            //Setting items again (this is needed for correct items id)
+            for (let i = 0; i <= itemsDeleteArray.length - 1; i++){
+                localStorage.setItem(`${itemsDeleteArray[i].id}`,
+                    JSON.stringify([itemsDeleteArray[i].id,
+                        itemsDeleteArray[i].itemText,
+                        itemsDeleteArray[i].isCompleted,
+                        itemsDeleteArray[i].currentDate]))
             }
             return {
+                //Setting items to UI
                 ...state,
-                items: newResultArray,
+                items: itemsDeleteArray,
             }
         case COMPLETE_ITEM:
+            //Getting items from localStorage and rewriting them with conditions
             for (let i = 1; i <= localStorage.length; i++){
-                let resultString = JSON.parse(localStorage.getItem(String(i)))
+                const resultString = JSON.parse(localStorage.getItem(String(i)))
                 if(resultString[0] === action.itemId){
                     if(resultString[2] === false){
-                        localStorage.setItem(`${resultString[0]}`, JSON.stringify([resultString[0], resultString[1], true, resultString[3]]))
+                        localStorage.setItem(`${resultString[0]}`,
+                            JSON.stringify(
+                                [resultString[0],
+                                resultString[1],
+                                true,
+                                resultString[3]])
+                        )
                     } else if(resultString[2] === true){
-                        localStorage.setItem(`${resultString[0]}`, JSON.stringify([resultString[0], resultString[1], false, resultString[3]]))
+                        localStorage.setItem(`${resultString[0]}`,
+                            JSON.stringify(
+                                [resultString[0],
+                                resultString[1],
+                                false,
+                                resultString[3]])
+                        )
                     }
                 }
             }
-            let itemsArray = [...state.items]
+            let itemsCompleteArray = [...state.items]
             for (let i = 0; i <= state.items.length - 1; i++) {
-                if(itemsArray[i].id === action.itemId){
-                    itemsArray[i].isCompleted = true
-                        ? itemsArray[i].isCompleted === false
-                        : itemsArray[i].isCompleted === true
+                if(itemsCompleteArray[i].id === action.itemId){
+                    itemsCompleteArray[i].isCompleted = true
+                        ? itemsCompleteArray[i].isCompleted === false
+                        : itemsCompleteArray[i].isCompleted === true
                 }
             }
             return {
+                //Setting items to UI
                 ...state,
                 items: [...state.items]
             }
         case COMPLETE_ALL_ITEMS:
+            //Clearing localStorage
             localStorage.clear()
             return {
+                //Setting items to UI
                 ...state,
                 items: []
             }
@@ -86,13 +131,20 @@ const todoReducer = (state = initialState, action) => {
 
 export default todoReducer
 
-export const getItems = () => ( { type: GET_ITEMS } )
 
-export const addItem = (itemText, currentDate) => ( { type: ADD_ITEM, itemText, currentDate} )
+//Action creators
+export const getItems = () =>
+    ( { type: GET_ITEMS } )
 
-export const deleteItem = (itemId) => ( { type: DELETE_ITEM, itemId } )
+export const addItem = (itemText) =>
+    ( { type: ADD_ITEM, itemText} )
 
-export const completeItem = (itemId) => ( { type: COMPLETE_ITEM, itemId } )
+export const deleteItem = (itemId) =>
+    ( { type: DELETE_ITEM, itemId } )
 
-export const completeAllItem = () => ( { type: COMPLETE_ALL_ITEMS } )
+export const completeItem = (itemId) =>
+    ( { type: COMPLETE_ITEM, itemId } )
+
+export const completeAllItem = () =>
+    ( { type: COMPLETE_ALL_ITEMS } )
 
