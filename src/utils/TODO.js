881e1fa7e998e656aa52a55962ef7065a60ee6df
Nikita Borovkov <72877821+Nickborovkov@users.noreply.github.com>
2021-08-01 21:12:05 +0300

    Update TODO.js

--- src/utils/TODO.js
+++ src/utils/TODO.js
@@ -1,4 +1,6 @@
-// TODO add form errors disappearing in 5 sec
 // TODO add @media's
 // TODO replace redux-form with formik
+// TODO add form errors disappearing in 5 sec
 // TODO Rewrite app with hooks and only functional components
+// TODO Add language change
+// TODO Add theme change
