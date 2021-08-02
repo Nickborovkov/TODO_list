bb620ec08354e2f33e5460e54a0502c35e114133
Nickborovkov <borovckov@yandex.ru>
2021-08-02 16:51:19 +0300

    deleted redux-form

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import {combineReducers, createStore} from "redux";
 import todoReducer from "./todoReducer";
-import {reducer as formReducer} from 'redux-form'
 
 const reducers = combineReducers({
     todo: todoReducer,
-    form: formReducer,
 })
 
 
