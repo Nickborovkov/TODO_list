550a0abadf1e1f591392191b94a8976a7006f584
Nickborovkov <borovckov@yandex.ru>
2021-08-02 16:49:33 +0300

    update TODO.js

--- src/TODO/TODO.js
+++ src/TODO/TODO.js
@@ -1,6 +1,3 @@
-// TODO add @media's
-// TODO replace redux-form with formik
-// TODO add form errors disappearing in 5 sec
 // TODO Rewrite app with hooks and only functional components
 // TODO Add language change
 // TODO Add theme change
--- src/utils/formHelpers/validators.js
+++
@@ -1,9 +0,0 @@
-export const required = (value) => {
-    if (!value) return `Goal can't be empty`
-    return undefined
-}
-
-export const maxLength = (value) => {
-    if(value.length > 300) return `Max length is 300 symbols`
-    return undefined
-}
