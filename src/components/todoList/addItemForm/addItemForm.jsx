3428db48d5d79a3704528e22ba7503c5b2cd144d
Nickborovkov <borovckov@yandex.ru>
2021-08-03 11:25:51 +0300

    error div css styles fix

--- src/components/todoList/addItemForm/addItemForm.jsx
+++ src/components/todoList/addItemForm/addItemForm.jsx
@@ -8,7 +8,10 @@ import * as yup from 'yup'
 const AddNewItem = ({addItem}) => {
 
     const validationSchema = yup.object().shape({
-        itemText: yup.string().required(`Goal can't be empty`).max(300, `Max length is 300 symbols`)
+        itemText: yup
+            .string()
+            .required(`Goal can't be empty`)
+            .max(300, `Max length is 300 symbols`)
     })
 
     return (
@@ -27,6 +30,8 @@ const AddNewItem = ({addItem}) => {
                 { ({ values, errors, touched, handleChange, handleBlur, handleSubmit,
                    isValid, dirty}) => (
                     <div className={cn(styles.form, media.form)}>
+                        {touched.itemText && errors.itemText &&
+                        <div className={cn(styles.errorArea, media.errorArea)}>{errors.itemText}</div>}
                         <input
                             className={cn(styles.input, media.input)}
                             type = 'text'
@@ -36,8 +41,7 @@ const AddNewItem = ({addItem}) => {
                             value={values.itemText}
                             placeholder='Add a goal here...'/>
 
-                        {touched.itemText && errors.itemText &&
-                        <div>{errors.itemText}</div>}
+
 
                         <button className={cn(styles.button, media.button)}
                                 disabled={!isValid && !dirty}
