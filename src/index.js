9a13653ddcfe7acb2c9866fe0d564bdb7592b6e1
Nikita Borovkov <borovckov@yandex.ru>
2021-08-28 22:35:14 +0300

    removed unused files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import store from "./redux/store";
 
@@ -11,5 +10,3 @@ ReactDOM.render(
         <App/>
     </Provider>, document.getElementById('root')
 );
-
-reportWebVitals();
